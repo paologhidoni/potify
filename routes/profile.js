d67b0e82cfb87978cc284c0dfd54346bcff2ede7
Paolo <15671939+paologhidoni@users.noreply.github.com>
2022-01-27 19:59:20 +0000

    Add post
    Co-authored-by: Oli <78092825+duckRabbitPy@users.noreply.github.com>
    Co-authored-by: Adam <31065348+adam8-9@users.noreply.github.com>

--- routes/profile.js
+++ routes/profile.js
@@ -1,41 +1,18 @@
 const model = require("../database/model.js");
 
-const get = (request, response) => {
 
-  model
-    .getUserPosts()
-    .then((result) => {
-      const userPosts = result.map((post) => {
-        return `
-        <li>
-          <h2 class="post__user">${post.username}</h2>
-          <img src="${post.imgSrc}" class="post__image">    
-          <p class="post__message">${post.post}</p>
-        </li>`;
-      });
 
-      return `
-      
-      <!DOCTYPE html>
-      <html lang="en">
-      <head>
-        <meta charset="UTF-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link rel="stylesheet" href="styles.css">
-        <title>Potify - Add Post</title>
-      </head>
-      <body>
+function get(request, response) {
+  const sid = request.signedCookies.sid;
+  if (sid) {
+    model.getSession(sid).then((session) => {
+      response.send(`
+        <h1>Hello ${session.user.email}</h1>
+        <form action="/sign-out" method="POST">
+          <button>Log out</button>
+        </form>
 
-        <div class="wrapper">
-
-          <section class="links">
-
-            <a href="/" class="btn">Back to Home</a>
-
-          </section>
-
-          <form action="/add-post" method="POST">
+        <form action="/add-post" method="POST">
 
             <label for="comment">Comment</label>
             <textarea name="comment" aria-label="Enter your comment" id="comment" cols="30" rows="5" required></textarea>
@@ -47,24 +24,86 @@ const get = (request, response) => {
 
           </form>
 
-          <ul class="user-posts">${userPosts}</ul>
-
-        </div>
-        
-      </body>
-      </html>
-
-      `;
-    })
-    .then((result) => {
-      response.send(result);
-    })
-    .catch((error) => {
-      console.log(error);
-      response.send("server error");
+      `);
     });
+  } else {
+    response.send(`
+    <h1>Hello anonymous</h1>
+    <a href="/sign-up">Sign up</a>
+    <span> | </span>
+    <a href="/log-in">Log in</a>
+  `);
+  }
+}
 
-};
+
+
+
+// const get = (request, response) => {
+
+//   model
+//     .getUserPosts()
+//     .then((result) => {
+//       const userPosts = result.map((post) => {
+//         return `
+//         <li>
+//           <h2 class="post__user">${post.username}</h2>
+//           <img src="${post.imgSrc}" class="post__image">    
+//           <p class="post__message">${post.post}</p>
+//         </li>`;
+//       });
+
+//       return `
+      
+//       <!DOCTYPE html>
+//       <html lang="en">
+//       <head>
+//         <meta charset="UTF-8">
+//         <meta http-equiv="X-UA-Compatible" content="IE=edge">
+//         <meta name="viewport" content="width=device-width, initial-scale=1.0">
+//         <link rel="stylesheet" href="styles.css">
+//         <title>Potify - Add Post</title>
+//       </head>
+//       <body>
+
+//         <div class="wrapper">
+
+//           <section class="links">
+
+//             <a href="/" class="btn">Back to Home</a>
+
+//           </section>
+
+//           <form action="/add-post" method="POST">
+
+//             <label for="comment">Comment</label>
+//             <textarea name="comment" aria-label="Enter your comment" id="comment" cols="30" rows="5" required></textarea>
+
+//             <label for="url">Image URL</label>
+//             <input type="text" aria-label="Enter a url to display an image of your plant" name="url" id="url" required> 
+
+//             <button type="submit" aria-label="click this button to send your post">Post your Plant</button>
+
+//           </form>
+
+//           <ul class="user-posts">${userPosts}</ul>
+
+//         </div>
+        
+//       </body>
+//       </html>
+
+//       `;
+//     })
+//     .then((result) => {
+//       response.send(result);
+//     })
+//     .catch((error) => {
+//       console.log(error);
+//       response.send("server error");
+//     });
+
+// };
 
 
 
@@ -87,10 +126,10 @@ const post = (request, response) => {
     let sanitisedInputs = sanitise(request.body.comment, request.body.url);
 
     const [comment, url] = sanitisedInputs;
-    console.log(result.user);
+    // console.log(result.user);
     model.createPost(result.user.username, comment, url, result.user.email)
     .then(() => {
-      response.send("/allPosts");
+      response.redirect("/allPosts");
     })  
   })
   .catch((error) => {
