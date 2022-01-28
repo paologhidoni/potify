404843f9d7970cd7b8ce631f97f6ac314c0f84a3
Paolo <15671939+paologhidoni@users.noreply.github.com>
2022-01-28 12:02:29 +0000

    heroku testing

--- database/connection.js
+++ database/connection.js
@@ -9,10 +9,11 @@ if (!DB_URL) {
   throw new Error("Please set the DATABASE_URL environment variable");
 }
 
-const options = {
-  connectionString: DB_URL,
-};
-
-const db = new pg.Pool(options);
+const db = new pg.Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
 
 module.exports = db;
